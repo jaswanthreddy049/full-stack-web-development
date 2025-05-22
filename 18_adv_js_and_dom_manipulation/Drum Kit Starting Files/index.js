a922685d6b57c7a7ce90cfaf9aec27f1f109f554
Likitha Maddipati <jaswanthreddy1979@gmail.com>
2025-05-22 12:54:46 +0530

    add new file

---
+++ 18_adv_js_and_dom_manipulation/Drum Kit Starting Files/index.js
@@ -0,0 +1,36 @@
+var n=document.querySelectorAll(".drum").length;
+for(var i=0;i<n;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var op=this.innerHTML;
+        switch(op){
+            case "w":
+                var music=new Audio("./sounds/kick-bass.mp3");
+                music.play();
+                break;
+            case "a":
+                var music=new Audio("./sounds/crash.mp3");
+                music.play();
+                break;
+            case "s":
+                var music=new Audio("./sounds/snare.mp3");
+                music.play();
+                break;
+            case "d":
+                var music=new Audio("./sounds/tom-1.mp3");
+                music.play();
+                break;
+            case "j":
+                var music=new Audio("./sounds/tom-2.mp3");
+                music.play();
+                break;
+            case "k":
+                var music=new Audio("./sounds/tom-3.mp3");
+                music.play();
+                break;
+            case "l":
+                var music=new Audio("./sounds/tom-4.mp3");
+                music.play();
+                break;
+        }
+});
+}
